4a426fdfdb19b5a086d3111690b1ca1ca9281b8d
AdishwarS <Adishwar.Suresh@infinite.com>
2025-07-21 02:01:04 -0500

    Add src/main/java/javaapiservice/Application.java

---
+++ src/main/java/javaapiservice/Application.java
@@ -0,0 +1,11 @@
+package javaapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaapiserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaapiserviceApplication.class, args);
+    }
+}
